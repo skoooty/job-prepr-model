c7371df5e9f14a3b625bbeba3e53cc334717a9bf
Martin Skalicky <martin.skalicky@theifactory.com>
2022-09-06 11:00:36 +0100

    CNN model shell structure done

---
+++ job_prepr_model/interface/main.py
@@ -0,0 +1,21 @@
+
+
+
+def preprocess(source_type='train'):
+    pass
+
+def train():
+    pass
+
+def evaluate():
+    pass
+
+def pred(X_pred):
+    pass
+
+if __name__ == '__main__':
+    preprocess()
+    preprocess(source_type='val')
+    train()
+    pred()
+    evaluate()
---
+++ job_prepr_model/ml_logic/model.py
@@ -0,0 +1,17 @@
+def initialize_model(X):
+    pass
+
+def compile_model(model, learning_rate):
+   pass
+
+def train_model(model:,
+                X,
+                y,
+                batch_size=64,
+                patience = 2,
+                validatoin_split=0.2
+                ):
+    pass
+
+def evaluate_model(model, X, y, batch_size=64):
+    pass
