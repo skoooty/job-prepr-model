1759a0d4cad95a8e851d97ecbb252e5c991bcdba
Martin Skalicky <martin.skalicky@theifactory.com>
2022-09-06 11:47:48 +0100

    CNN model.py data.py 1st version from notebook, params hardcoded

--- job_prepr_model/ml_logic/data.py
+++ job_prepr_model/ml_logic/data.py
@@ -1,2 +1,21 @@
+import pandas as pd
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+import os
+
+train_path = "/Users/andrei/code/images/images/train"
+test_path = "/Users/andrei/code/images/images/validation"
+val_path = "~/code/images/images/val"
+
 def load_data():
-    pass
+
+    #all_images = []
+    y_train = []
+    X_train = []
+    for folder_path in os.listdir(train_path):
+        if not folder_path.startswith("."):
+            for image_path in os.listdir(os.path.join(train_path, folder_path)):
+                img = load_img(os.path.join(os.path.join(train_path, folder_path), image_path), color_mode = "grayscale")
+                X_train.append(img_to_array(img))
+                y_train.append(os.path.basename(os.path.normpath(folder_path)))
+    return X_train, y_train
--- job_prepr_model/ml_logic/model.py
+++ job_prepr_model/ml_logic/model.py
@@ -1,17 +1,53 @@
-def initialize_model(X):
-    pass
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras import layers, models
+from tensorflow.keras.callbacks import EarlyStopping
+
+from job_prepr_model.ml_logic.data import load_data
+
+X_train, y_train = load_data()
+
+def initialize_model():
+
+    model = models.Sequential()
+
+    # Notice this cool new layer that "pipe" your rescaling within the architecture
+    model.add(Rescaling(1./255, input_shape=(48, 48, 1)))
+
+    # Lets add 3 convolution layers, with relatively large kernel size as our pictures are quite big too
+    model.add(layers.Conv2D(16, kernel_size=5, activation='relu'))
+    model.add(layers.MaxPooling2D(3))
+
+    model.add(layers.Conv2D(32, kernel_size=3, activation="relu"))
+    model.add(layers.MaxPooling2D(3))
+
+    model.add(layers.Conv2D(32, kernel_size=2, activation="relu"))
+    model.add(layers.MaxPooling2D(3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(100, activation='relu'))
+    model.add(layers.Dense(7, activation='softmax'))
+    return model
 
 def compile_model(model, learning_rate):
-   pass
+   model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+   return model
 
-def train_model(model:,
+def train_model(model,
                 X,
                 y,
                 batch_size=64,
                 patience = 2,
-                validatoin_split=0.2
+                validation_split=0.2
                 ):
-    pass
+    history = model.fit(X, y,
+              batch_size=batch_size, epochs = 1000,
+              callbacks=[EarlyStopping(patience = patience, restore_best_weights= True, monitor = "val_accuracy", mode = "max")],
+              validation_split = validation_split, verbose = 1)
+
+    return model, history
 
 def evaluate_model(model, X, y, batch_size=64):
-    pass
+    metrics = model.evaluate(X, y)
+    return metrics
