026d2d6d8a3dc1959602bb2cf7698a1be3a2afcc
Andrei Danila <andrei.c.danila@gmail.com>
2022-09-07 10:37:00 +0100

    added the gcp sample code

---
+++ job_prepr_model/api/transcription.py
@@ -0,0 +1,25 @@
+from google.cloud import speech_v1 as speech
+
+
+def speech_to_text(config, audio):
+    client = speech.SpeechClient()
+    response = client.recognize(config=config, audio=audio)
+    print_sentences(response)
+
+
+def print_sentences(response):
+    for result in response.results:
+        best_alternative = result.alternatives[0]
+        transcript = best_alternative.transcript
+        confidence = best_alternative.confidence
+        print("-" * 80)
+        print(f"Transcript: {transcript}")
+        print(f"Confidence: {confidence:.0%}")
+
+
+#Example call to api
+'''
+config = dict(language_code="en-UK")
+config.update(dict(enable_automatic_punctuation=True))
+audio = dict(uri="gs://cloud-samples-data/speech/brooklyn_bridge.flac")
+'''
