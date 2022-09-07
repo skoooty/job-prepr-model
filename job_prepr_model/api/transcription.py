4029d130fa091b52f93bac00470f73b688e5d359
Andrei Danila <andrei.c.danila@gmail.com>
2022-09-07 11:58:51 +0100

    refinement of transcription function

--- job_prepr_model/api/transcription.py
+++ job_prepr_model/api/transcription.py
@@ -1,12 +1,26 @@
 from google.cloud import speech_v1 as speech
 import io
+import os
 
-def transcribe_from_bucket(uri):
-    '''
-    Pass this function your audio file and the config and you will get a transcription
-    '''
-    config = dict(language_code="en-UK", enable_automatic_punctuation=True)
-    audio = dict(uri=uri)
+
+def transcribe(source):
+    """Transcribe the given audio file from a local or bucket path"""
+    if os.environ.get("TRANSCRIPTION_SOURCE") == "local":
+        client = speech.SpeechClient()
+
+        with io.open(source, "rb") as audio_file:
+            content = audio_file.read()
+
+        audio = speech.RecognitionAudio(content=content)
+        config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        language_code="en-US",
+        audio_channel_count=2
+        )
+    else:
+        config = dict(language_code="en-UK", enable_automatic_punctuation=True)
+        audio = dict(uri=source)
+
     client = speech.SpeechClient()
     response = client.recognize(config=config, audio=audio)
     for result in response.results:
@@ -14,32 +28,4 @@ def transcribe_from_bucket(uri):
     transcript = best_alternative.transcript
     print(transcript)
 
-
-
-def transcribe_from_local(path):
-    """Transcribe the given audio file from a local path"""
-    from google.cloud import speech
-    import io
-
-    client = speech.SpeechClient()
-
-    with io.open(path, "rb") as audio_file:
-        content = audio_file.read()
-
-    audio = speech.RecognitionAudio(content=content)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        language_code="en-US",
-        audio_channel_count=2
-    )
-
-    response = client.recognize(config=config, audio=audio)
-
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
-    for result in response.results:
-        # The first alternative is the most likely one for this portion.
-        print(u"Transcript: {}".format(result.alternatives[0].transcript))
-    return result.alternatives[0].transcript
-
-transcribe_from_local("/Users/andrei/Downloads/you-are-acting-so-weird.wav")
+transcribe("/Users/andrei/Downloads/you-are-acting-so-weird.wav")
