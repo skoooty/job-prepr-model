4f4a2f3a71382dc53f8a7109c04615ea9a774235
Andrei Danila <andrei.c.danila@gmail.com>
2022-09-07 13:23:31 +0100

    finished transcription

--- job_prepr_model/api/transcription.py
+++ job_prepr_model/api/transcription.py
@@ -2,7 +2,6 @@ from google.cloud import speech_v1 as speech
 import io
 import os
 
-
 def transcribe(source):
     """Transcribe the given audio file from a local or bucket path"""
     if os.environ.get("TRANSCRIPTION_SOURCE") == "local":
@@ -15,7 +14,7 @@ def transcribe(source):
         config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         language_code="en-US",
-        audio_channel_count=2
+        audio_channel_count=1
         )
     else:
         config = dict(language_code="en-UK", enable_automatic_punctuation=True)
@@ -26,6 +25,4 @@ def transcribe(source):
     for result in response.results:
         best_alternative = result.alternatives[0]
     transcript = best_alternative.transcript
-    print(transcript)
-
-transcribe("/Users/andrei/Downloads/you-are-acting-so-weird.wav")
+    return transcript
