0cd6de2dcbf9de9bd957317fe515c9819b301cb8
Huw Barber <huwbarber@gmail.com>
2022-09-08 12:30:18 +0100

    nlp api

--- job_prepr_model/api/nlp_api.py
+++ job_prepr_model/api/nlp_api.py
@@ -4,12 +4,6 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 app.state.model = pipeline('sentiment-analysis', model='siebert/sentiment-roberta-large-english')
 
-@app.get("/hello")
-def hello():
-    return 'hello'
-
-
 @app.get("/predictnlp")
 async def predict_nlp(text):
-    print(app.state.model(text))
     return app.state.model(text)
